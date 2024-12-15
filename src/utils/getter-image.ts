13e2abfb9c3495dc7cdf8490e47014ec7e17b992
genbiuniska <abyalaxx@gmail.com>
2024-12-15 19:38:06 +0700

    Fixx error path image not found at build vercel

--- src/utils/getter-image.ts
+++ src/utils/getter-image.ts
@@ -1,46 +1,44 @@
-import genBI from "/public/assets/img/logogenbi.png";
-import homepage from "/public/assets/img/homepage.jpg";
-import rapat from "/public/assets/img/rapat.jpg"
-import fotbar from "/public/assets/img/fotbar.jpg"
+import genBI from "@/assets/img/logogenbi.png";
+import homepage from "@/assets/img/homepage.jpg";
+import rapat from "@/assets/img/rapat.jpg";
+import fotbar from "@/assets/img/fotbar.jpg";
 
-import a from "/public/assets/img/a.jpg";
+import a from "@/assets/img/a.jpg";
 
-import p1 from "/public/assets/dummy/1.jpg"
-import p2 from "/public/assets/dummy/2.jpg"
-import p3 from "/public/assets/dummy/3.jpg"
-import p4 from "/public/assets/dummy/4.jpg"
-import p5 from "/public/assets/dummy/5.jpg"
-import p6 from "/public/assets/dummy/6.jpg"
-import p7 from "/public/assets/dummy/7.jpg"
-import p8 from "/public/assets/dummy/8.jpg"
-import p9 from "/public/assets/dummy/9.jpg"
-import p10 from "/public/assets/dummy/10.jpg"
+import p1 from "@/assets/dummy/1.jpg";
+import p2 from "@/assets/dummy/2.jpg";
+import p3 from "@/assets/dummy/3.jpg";
+import p4 from "@/assets/dummy/4.jpg";
+import p5 from "@/assets/dummy/5.jpg";
+import p6 from "@/assets/dummy/6.jpg";
+import p7 from "@/assets/dummy/7.jpg";
+import p8 from "@/assets/dummy/8.jpg";
+import p9 from "@/assets/dummy/9.jpg";
+import p10 from "@/assets/dummy/10.jpg";
 
-import ba from "/public/assets/kegiatan/temuResponden/a.jpeg";
-import bb from "/public/assets/kegiatan/temuResponden/b.jpeg";
-import bc from "/public/assets/kegiatan/temuResponden/c.jpeg";
-import bd from "/public/assets/kegiatan/temuResponden/d.jpeg";
-import be from "/public/assets/kegiatan/temuResponden/e.jpeg";
-import bf from "/public/assets/kegiatan/temuResponden/f.jpeg";
+import ba from "@/assets/kegiatan/a.jpeg";
+import bb from "@/assets/kegiatan/b.jpeg";
+import bc from "@/assets/kegiatan/c.jpeg";
+import bd from "@/assets/kegiatan/d.jpeg";
+import be from "@/assets/kegiatan/e.jpeg";
+import bf from "@/assets/kegiatan/f.jpeg";
 
-const temuResponden = [ba, bb, bc, bd, be, bf]
+const temuResponden = [ba, bb, bc, bd, be, bf];
 
 const uniska = {
     fotbar
-}
+};
 
 const random = {
     a
-}
-
+};
 
 const kegiatan = {
     temuResponden
-}
+};
 
 const dummyProfile = {
     p1, p2, p3, p4, p5, p6, p7, p8, p9, p10
-}
+};
 
-export { genBI, homepage, rapat, random, uniska, dummyProfile, kegiatan }
-
+export { genBI, homepage, rapat, random, uniska, dummyProfile, kegiatan };
